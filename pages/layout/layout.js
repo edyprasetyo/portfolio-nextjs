9e4e29efc9a9dcefe5acc10c7cbd40392f7e7ef3
csmcorporatama@gmail.com <indorent10>
2022-03-21 17:26:54 +0700

    dasd

--- pages/layout/layout.js
+++ pages/layout/layout.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Script from "next/script";
 import React from "react";
 import Footer from "./footer";
 import Header from "./header";
@@ -11,12 +12,9 @@ export default function Layout({ children }) {
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
                 <link rel="icon" type="image/x-icon" href="/images/logo_1.ico" />
 
-                <script src="https://code.iconify.design/2/2.1.2/iconify.min.js"></script>
-
-
-                <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/highlight.min.js"></script>
-
-                <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+                <Script src="https://code.iconify.design/2/2.1.2/iconify.min.js" />
+                <Script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/highlight.min.js" />
+                <Script src="https://unpkg.com/aos@2.3.1/dist/aos.js" />
 
             </Head>
             <Header />
