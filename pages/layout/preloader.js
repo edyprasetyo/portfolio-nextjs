f736c0b7c8004094817e548321cf39d643346c28
csmcorporatama@gmail.com <indorent10>
2022-03-26 05:16:25 +0700

    dasdsa

--- pages/layout/preloader.js
+++ pages/layout/preloader.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Loading({ isLoading }) {
-    return isLoading && (
+    return isLoading ? (<span></span>) : (
         <div className="pre-loader">
             <div className="sk-fading-circle">
                 <img id="imgPreloader" className="rotate" src="/images/logo.png" />
