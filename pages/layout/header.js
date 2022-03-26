e24048400a11f75d1f547e3d540f2ad2238e8a81
csmcorporatama@gmail.com <indorent10>
2022-03-27 00:30:51 +0700

    dsadsad

--- pages/layout/header.js
+++ pages/layout/header.js
@@ -1,19 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
 
 function Header() {
+    const [expanded, setExpanded] = useState(false);
+
+
     return (
-        <Navbar bg="light" expand="lg" className="navbar fixed-top navbar-expand-lg navbar-light">
+        <Navbar expanded={expanded} bg="light" expand="lg" className="navbar fixed-top navbar-expand-lg navbar-light">
             <Container>
                 <Navbar.Brand href="#home"><img src="/images/logo.png" className="logo" alt="app logo" /></Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Toggle onClick={() => setExpanded(true)} aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#about-me">About Me</Nav.Link>
-                        <Nav.Link href="#skills">Skills</Nav.Link>
-                        <Nav.Link href="#latest-projects">Projects</Nav.Link>
-                        <Nav.Link href="#contact-me">Contact Me</Nav.Link>
+                        <Nav.Link onClick={() => setExpanded(false)} href="#home">Home</Nav.Link>
+                        <Nav.Link onClick={() => setExpanded(false)} href="#about-me">About Me</Nav.Link>
+                        <Nav.Link onClick={() => setExpanded(false)} href="#skills">Skills</Nav.Link>
+                        <Nav.Link onClick={() => setExpanded(false)} href="#latest-projects">Projects</Nav.Link>
+                        <Nav.Link onClick={() => setExpanded(false)} href="#contact-me">Contact Me</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
