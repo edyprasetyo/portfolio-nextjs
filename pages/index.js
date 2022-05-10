1ce8132c2242725f8a6f1531cbd4e6120b1ca869
csmcorporatama@gmail.com <indorent10>
2022-05-10 10:58:11 +0700

    tes

--- pages/index.js
+++ pages/index.js
@@ -153,6 +153,7 @@ export default function Home() {
             </div>
             <div className="col-lg-6 div-parent-terminal">
               <div className="h1 text-green mb-2">
+                tes
                 <strong>
                   <Terminal
                     lines={termLines}
