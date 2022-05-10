6abb5bb67abe8688e67056d58d860dd313da89cd
csmcorporatama@gmail.com <indorent10>
2022-05-10 14:10:57 +0700

    dsadsa

--- pages/index.js
+++ pages/index.js
@@ -153,6 +153,7 @@ export default function Home() {
             </div>
             <div className="col-lg-6 div-parent-terminal">
               <div className="h1 text-green mb-2">
+
                 <strong>
                   <Terminal
                     lines={termLines}
