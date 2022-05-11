dc0c10b3f98f50a288d14a7a9ca837a34c208750
csmcorporatama@gmail.com <indorent10>
2022-05-11 22:32:38 +0700

    run ssh

---
+++ deploy_to_aws.sh
@@ -0,0 +1,8 @@
+echo 'Starting to Deploy...'
+ssh ec2-user@34.222.141.246 " 
+        cd /usr/local/src/portfolio-nextjs
+        git pull
+        docker build portfolio-nextjs
+        docker-compose up --no-deps -d portfolio-nextjs
+        "
+echo 'Deployment completed successfully'
